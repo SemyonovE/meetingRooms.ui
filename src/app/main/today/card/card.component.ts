b2a5f9626828936ffbdcef8a377b4978af373442
Evgenij <semenov.evgeny.92@gmail.com>
2018-03-11 11:16:49 +0700

    Today style with free events

--- src/app/main/today/card/card.component.ts
+++ src/app/main/today/card/card.component.ts
@@ -7,11 +7,18 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() meeting;
+  @Input() freeDuration;
+
+  meetingSeconds: number = 0;
+
+  time: Date = new Date();
 
   constructor() {
   }
 
   ngOnInit() {
+    this.meetingSeconds = Date.parse(this.meeting['EndDate'])
+    setInterval(() => this.time = new Date(), 1000);
   }
 
 }
--- src/app/main/today/today.component.ts
+++ src/app/main/today/today.component.ts
@@ -9,11 +9,24 @@ import {MeetingsService} from '../../meetings.service';
 export class TodayComponent implements OnInit {
   meetingService: MeetingsService;
 
+  // durations: number[];
+
   constructor(private service: MeetingsService) {
-    this.meetingService = service;
+    
+  }
+
+  getDuration(index) {
+    if(index == 0) return 0;
+    return (+Date.parse(this.meetingService.meetings[index]['StartDate']) - +Date.parse(this.meetingService.meetings[index - 1]['EndDate'])) / 60000
   }
 
   ngOnInit() {
+    this.meetingService = this.service;
+    // let meetings = this.meetingService.meetings;
+    // for (let i = 0; i < meetings.length - 1; i++) {
+    //   this.durations.push((+Date.parse(meetings[i + 1]['StartDate']) - +Date.parse(meetings[i]['EndDate'])) / 60);
+    // }
+    // this.durations.push(0);
   }
 
 }
